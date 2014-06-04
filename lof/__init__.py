512300b55e7cfc4be03fc9f4d2e411710df71563
absolutelyNoWarranty <computerholic5568@gmail.com>
2014-06-04 17:09:16 +0800

    Add lof

---
+++ lof/__init__.py
@@ -0,0 +1,1 @@
+from .lof import LOF
---
+++ lof/lof.py
@@ -0,0 +1,46 @@
+from ..base import BaseAnomalyDetector
+
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+class LOF(BaseAnomalyDetector):
+    '''
+    Local Outlier Factor
+    '''
+    
+    def __init__(self, k):
+        self.k = k
+    
+    def predict(self, A):
+        '''
+        Predict anomaly scores for dataset X
+        '''
+        nbrs = NearestNeighbors(n_neighbors=self.k).fit(A)
+        distances, indices = nbrs.kneighbors(A)
+        indices = indices[:, 1:]
+        distances = distances[:, 1:]
+        
+        k_dists = distances[:, -1]
+        
+        num_rows = A.shape[0]
+        
+        lrd_value = np.zeros((num_rows, 1));
+        lrd_value = np.zeros(num_rows)
+        for i in xrange(num_rows):
+            temp = A[i, :] - A[indices[i], :]
+            temp = np.sqrt(np.sum(temp**2, 1))
+            reachability_dists = np.max(np.vstack([temp, k_dists[indices[i]]]), 0)
+            lrd_value[i] = self.k/sum(reachability_dists);
+        
+        lof = np.zeros(num_rows)
+        for i in xrange(num_rows):
+            lof[i] = np.sum(lrd_value[indices[i]]) / lrd_value[i] / self.k
+        
+        return(lof)
+    
+    def fit(self, A=None, y=None):
+        self.A_ = A
+        return self
+    
+    #def lrd(A, index_p, k_dist, k_index, numneighbors):
+    #    temp = A[index_p, :] - A[k_index[]]
