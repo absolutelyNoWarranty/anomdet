2c4f641b32dc531254ab089ea18e0ecaf9eb22f9
absolutelyNoWarranty <computerholic5568@gmail.com>
2014-06-11 15:24:08 +0800

    Add lof documentation

--- lof/lof.py
+++ lof/lof.py
@@ -1,20 +1,49 @@
+# -*- coding: utf-8 -*-
+
 from ..base import BaseAnomalyDetector
 
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
 class LOF(BaseAnomalyDetector):
-    '''
-    Local Outlier Factor
-    '''
+    """Calculate Local Outlier Factor of data
+
+    Parameters
+    ----------
+
+    k : int
+        Number of nearest neighbors to use
+
+    Returns
+    -------
+
+    cluster_centers_indices : array, shape (n_clusters,)
+        index of clusters centers
+
+    labels : array, shape (n_samples,)
+        cluster labels for each point
+
+    References
+    ----------
+    Markus M. Breunig, Hans-Peter Kriegel, Raymond T. Ng, and Jörg Sander. 2000. LOF: identifying density-based local outliers. SIGMOD Rec. 29, 2 (May 2000), 93-104. DOI=10.1145/335191.335388 http://doi.acm.org/10.1145/335191.335388
+    """
     
     def __init__(self, k):
         self.k = k
     
     def predict(self, A):
-        '''
-        Predict anomaly scores for dataset X
-        '''
+        """Calculate local outlier factor for each sample in A
+
+        Parameters
+        ----------
+        A : array-like, shape (n_samples, n_features)
+            New data to predict.
+
+        Returns
+        -------
+        lof : array, shape (n_samples,)
+            Local outlier factor for each sample.
+        """
         nbrs = NearestNeighbors(n_neighbors=self.k+1).fit(A)
         distances, indices = nbrs.kneighbors(A)
         indices = indices[:, 1:]
