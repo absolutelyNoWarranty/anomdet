8e2bf51ca67dab9de72209ec54b28e5490874c03
absolutelyNoWarranty <computerholic5568@gmail.com>
2015-02-27 22:34:18 +0800

    Update normalize to work with matrix of outlier scores

--- utils/normalize_scores.py
+++ utils/normalize_scores.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def normalize_scores(x, method='uniform'):
+def normalize_scores(X, method='uniform'):
     '''
     Normalize outlier scores
     Inputs:
-        x : a vector of anomaly scores
+        X : matrix of outlier scores, each column being a set of scores from one same method
         method : (default: 'uniform')
             'uniform' - a linear transformation to the range [0, 1]
             'gaussian' - gaussian distribution
@@ -16,6 +16,12 @@ def normalize_scores(x, method='uniform'):
     '''
     
     if method == 'uniform':
-        norm_scores = (x - np.min(x)) / (np.max(x) - np.min(x))
+        mins = np.min(X, axis=0)
+        maxs = np.max(X, axis=0)
+        ranges = maxs-mins
+        zero = np.where(ranges == 0.)[0]
+        if len(zero):
+            raise Exception("Range 0! Columns: %s" % str(list(zero)))
+        norm_scores = (X - mins) / (ranges)
     
     return norm_scores
