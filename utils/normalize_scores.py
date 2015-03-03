5e9ea8ada616fb20cc749c82a565c25796cf9729
absolutelyNoWarranty <computerholic5568@gmail.com>
2015-03-03 20:07:54 +0800

    utils/normalize_scores.py

--- utils/normalize_scores.py
+++ utils/normalize_scores.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def normalize_scores(X, method='uniform'):
+def normalize_scores(X, method='uniform', best_effort=True):
     '''
     Normalize outlier scores
     Inputs:
@@ -10,6 +10,7 @@ def normalize_scores(X, method='uniform'):
             'gaussian' - gaussian distribution
             'gamma' - gamma distribution 
             'f' - F-distribution
+        best_effort : Don't raise exception
     Output: the normalized scores
     
     See: Kriegel et al. "Interpreting and Unifying Outlier Scores"
@@ -17,12 +18,18 @@ def normalize_scores(X, method='uniform'):
     
     if method == 'uniform':
         # ignore nans
-        mins = np.nanmin(X, axis=0)
-        maxs = np.nanmax(X, axis=0)
+        mins = np.array(np.min(np.ma.masked_invalid(X), axis=0))
+        maxs = np.array(np.max(np.ma.masked_invalid(X), axis=0))
         ranges = maxs-mins
         zero = np.where(ranges == 0.)[0]
         if len(zero):
-            raise Exception("Range 0! Columns: %s" % str(list(zero)))
+            if best_effort:
+                ranges[zero] = 1.0
+                # will turn everything to 0 since it's minus mins which is the same for everything
+                #raise Warning("Range is 0! Columns: %s" % str(list(zero)))
+            else:
+                raise Exception("Range is 0! Columns: %s" % str(list(zero)))
+            
         norm_scores = (X - mins) / (ranges)
     
     return norm_scores
