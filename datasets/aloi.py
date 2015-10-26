3009347d3b32cf1a3f9d8e2cca8a672b486bb571
absolutelyNoWarranty <computerholic5568@gmail.com>
2015-10-27 00:33:45 +0800

    Change aloi to use OutlierDataset

--- datasets/aloi.py
+++ datasets/aloi.py
@@ -1,17 +1,13 @@
 import os
 import numpy as np
-from ..utils import unique_rows
 
+from .base import OutlierDataset
 
-def load_aloi(remove_duplicates=True):
+def load_aloi():
     X = np.loadtxt(os.path.join(os.path.dirname(__file__), 'aloi-27d-50000-max5-tot1508.csv.gz'),
                    usecols=range(27))
     y = 'Outlier' == np.loadtxt(os.path.join(os.path.dirname(__file__), 'aloi-27d-50000-max5-tot1508.csv.gz'),
                      usecols=[29],
                      dtype='str')
     
-    if remove_duplicates:
-        X, ind = unique_rows(X, return_index=True)
-        y = y[ind]
-        
-    return (X, y)
+    return OutlierDataset(X, y=y, name='ALOI', allow_duplicates=False)
