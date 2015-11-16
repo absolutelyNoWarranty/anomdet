948a422bb8f39fffd2a7631422fd9c28db730d65
absolutelyNoWarranty <computerholic5568@gmail.com>
2015-11-16 22:45:06 +0800

    Add normalize feature for OutlierDataset

--- datasets/base.py
+++ datasets/base.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 
 import numpy as np
 from sklearn.metrics import roc_auc_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import normalize
 
 from ..utils.dataset_info import DatasetInfo
 from ..utils.simple_timer import SimpleTimer
@@ -47,7 +48,29 @@ def iter_sampled_outliers(self, outlier_ratio=0.05, random_state=None, overlap=T
     
 class OutlierDataset(object):
     def __init__(self, X, y=None, name=None, pos_class_name="outlier",
-                 neg_class_name="normal", allow_duplicates=True):
+                 neg_class_name="normal", allow_duplicates=True, normalize_features=False):
+        '''
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_dimensions) 
+            the data
+        
+        y : array-like, boolean, shape (n_samples, )
+            the labels
+            
+        name : str, optional
+            user-friendly name for this dataset
+            
+        pos_class_name : str, optional (default="outlier")
+        
+        neg_class_name : str, optional (default="normal")
+        
+        allow_duplicates : str, optional (default="normal")
+        
+        normalize_features : bool, optional (default=False)
+            whether to normalize features (columns) when returning X
+            
+        '''
         if X.shape[0] != y.shape[0]:
             raise ValueError("Number of rows in X is not the same as length of "
                              "y.")
@@ -64,13 +87,20 @@ class OutlierDataset(object):
         self.timer = SimpleTimer()
         
         self.set_duplicates_allowed(allow_duplicates)
+        self.set_normalize_features(normalize_features)  ## TODO just accept a pipeline or something 
         
     @property
     def X(self):
-        if self.duplicates_allowed:
-            return self._X
+        if self._normalize_features:
+            if self.duplicates_allowed:
+                return normalize(self._X, axis=0)
+            else:
+                return normalize(self._X[self.unique_ind], axis=0)
         else:
-            return self._X[self.unique_ind]
+            if self.duplicates_allowed:
+                return self._X
+            else:
+                return self._X[self.unique_ind]
             
     @property
     def data(self):
@@ -91,6 +121,9 @@ class OutlierDataset(object):
         self.duplicates_allowed = is_ok
         if not is_ok:
             _, self.unique_ind = unique_rows(self._X, return_index=True)
+    
+    def set_normalize_features(self, bool_):
+        self._normalize_features = bool_
     
     def benchmark(self, thingy, k=None, threshold=None, verbose=False):
         '''
@@ -156,7 +189,41 @@ class OutlierDataset(object):
                                
         if verbose: print result
         return result
+    
+    def benchmark_individual_outliers(self, thingy, k=10, threshold=None, verbose=False):
+        '''
+        Given a `thingy`, benchmark it on this dataset by calling the thingy's
+        predict on the normal class+ one outlier at a time.
+        Return the average recall@k (which is zero/one) over all trials.
         
+        Parameters
+        ----------
+        thingy : something that can predict outlier scores
+        
+        k : int, optional (default=10)
+            The top k objects to consider.
+        
+        verbose : bool, (default=False)
+            Whether to print stuff while running.
+        
+        Returns
+        -------
+        average_recall : The average recal over n_outliers trials.
+            
+        '''
+        
+        pos_ind = np.where(self.y)[0]
+        neg_ind = np.where(np.logical_not(self.y))[0]
+        wins = 0.
+        for i in pos_ind:
+            X = self.X[np.append(neg_ind, i)]
+            preds = thingy.fit(X).predict(X)
+            top_k = np.argsort(preds)[-k:]
+            if len(neg_ind) in top_k:
+                wins += 1.
+        average_recall = wins / len(pos_ind)
+        return average_recall
+    
     def get_subsampled(self, anomaly_ratio=0.05, random_state=None):
         '''
         Return an OutlierDataset with
@@ -174,6 +241,9 @@ class OutlierDataset(object):
         Evaluate `preds` on this OutlierDataset using the AUC of the ROC curve.
         '''
         return roc_auc_score(self.labels, preds)
+    
+    def table(self, tablefmt="pipe"):
+        return self._info.calc(self.y).table(tablefmt=tablefmt)
     
     @property
     def info(self):
